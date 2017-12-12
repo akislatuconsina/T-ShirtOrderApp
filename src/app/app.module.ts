306aa98fa250a484c7ce7acb6867f0b55e3c148b
DESKTOP-C699GHK\HARYANTO SETYAWAN <haryantoset14@gmail.com>
2017-12-13 03:52:57 +0700

    dashboard and animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { IonicStorageModule } from '@ionic/storage';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MyApp } from './app.component';
 
@@ -18,6 +19,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     IonicStorageModule.forRoot(),
     IonicModule.forRoot(MyApp),
     SDKBrowserModule.forRoot(),
+    BrowserAnimationsModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, trigger, state, style, transition, animate, keyframes } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { OzanUserCredentialApi } from '../../shared/sdk/services/custom/OzanUserCredential';
 
@@ -13,8 +13,61 @@ import { OzanUserCredentialApi } from '../../shared/sdk/services/custom/OzanUser
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html',
+  animations: [
+    
+        //For the logo
+        trigger('flyInBottomSlow', [
+          state('in', style({
+            transform: 'translate3d(0,0,0)'
+          })),
+          transition('void => *', [
+            style({ transform: 'translate3d(0,2000px,0' }),
+            animate('2000ms ease-in-out')
+          ])
+        ]),
+    
+        //For the background detail
+        trigger('flyInBottomFast', [
+          state('in', style({
+            transform: 'translate3d(0,0,0)'
+          })),
+          transition('void => *', [
+            style({ transform: 'translate3d(0,2000px,0)' }),
+            animate('1000ms ease-in-out')
+          ])
+        ]),
+    
+        //For the login form
+        trigger('bounceInBottom', [
+          state('in', style({
+            transform: 'translate3d(0,0,0)'
+          })),
+          transition('void => *', [
+            animate('2000ms 200ms ease-in', keyframes([
+              style({ transform: 'translate3d(0,2000px,0)', offset: 0 }),
+              style({ transform: 'translate3d(0,-20px,0)', offset: 0.9 }),
+              style({ transform: 'translate3d(0,0,0)', offset: 1 })
+            ]))
+          ])
+        ]),
+    
+        //For login button
+        trigger('fadeIn', [
+          state('in', style({
+            opacity: 1
+          })),
+          transition('void => *', [
+            style({ opacity: 0 }),
+            animate('1000ms 2000ms ease-in')
+          ])
+        ])
+      ]
 })
 export class LoginPage {
+  public logoState: any = "in";
+  public cloudState: any = "in";
+  public loginState: any = "in";
+  public formState: any = "in";
   public username: any;
   public password: any;
 
