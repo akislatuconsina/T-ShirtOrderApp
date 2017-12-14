2425dcc7770427d591e036373c5a3cbc538fa917
atsun <adjietirta13@gmail.com>
2017-12-14 20:42:44 +0700

    data regostration, page forget password, and page report order

---
+++ src/pages/reportorder/reportorder.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ReportorderPage } from './reportorder';
+
+@NgModule({
+  declarations: [
+    ReportorderPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(ReportorderPage),
+  ],
+})
+export class ReportorderPageModule {}
