feb2d109e5ee33123e80e917d9734fddad0f2a1c
Rendy Sutandy <rendy1429@gmail.com>
2017-12-14 23:35:11 +0700

    Revert "Hotfix/vmd project1"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,48 +2,27 @@ import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { Storage } from '@ionic/storage';
-import { OzanUserCredentialApi } from '../shared/sdk/services/custom/OzanUserCredential';
+
+import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  
   @ViewChild(Nav) nav: Nav;
-  public accessTok: any;
-  public storageData: any;
-  rootPage: any = 'LoginPage';
 
-  pages: Array<{ title: string, component: any, icons: any }>;
-  public photo: any = 'assets/imgs/ojan.JPG';
+  rootPage: any = HomePage;
 
-  constructor(
-    public platform: Platform,
-    public statusBar: StatusBar,
-    public storage: Storage,
-    public splashScreen: SplashScreen,
-    public ozanusercredentialApi: OzanUserCredentialApi
-  ) {
+  pages: Array<{title: string, component: any}>;
 
-    this.storage.get('ozanStorage').then((ozanStorage) => {
-      if (ozanStorage == null || ozanStorage == undefined) {
-        this.rootPage = 'LoginPage';
-      } else {
-        this.nav.setRoot('HomePage');
-      }
-    });
-
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: 'HomePage', icons: 'home' },
-      { title: 'Order Online', component: 'OrderPage', icons: 'md-cart' },
-      { title: 'Order Detail', component: 'OrderdetailPage', icons: 'md-cart' },
-      { title: 'Laporan Order', component: 'TestpagePage', icons: 'md-code-download' },
-      { title: 'Pengaturan Akun', component: 'HomePage', icons: 'md-color-filter' },
-      // { title: 'Logout', component: 'HomePage', icons: 'home' }
+      { title: 'Home', component: HomePage },
+      { title: 'List', component: ListPage }
     ];
 
   }
@@ -61,20 +40,5 @@ export class MyApp {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
     this.nav.setRoot(page.component);
-  }
-  logout() {
-    this.storage.get('OzanUserCredential').then((result) => {
-      console.log(result);
-      this.storageData = result;
-      this.ozanusercredentialApi.ozanLogout(this.storageData).subscribe(result => {
-        console.log(result);
-        //Direct ke login page
-      }, (error) => {
-        console.log(error, 'error')
-      })
-    })
-    // this.ozanusercredentialApi.ozanLogout()
-    // this.storage.clear();
-    // this.nav.setRoot('LoginPage');
   }
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,36 +1,14 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
 
-@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  public username:any;
 
-  constructor(
-    public navCtrl: NavController,
-    public navParams: NavParams
-  ) {
+  constructor(public navCtrl: NavController) {
 
-    this.username = this.navParams.get('name'); //fungsi ini digunakan untuk menarik data variable
-    
-  }
-  login() {
-    this.navCtrl.push('LoginPage');
-  }
-
-  gotoOrderPage(){
-    this.navCtrl.push('OrderPage')
-  } 
-
-  gotoDetailOrder(){
-    this.navCtrl.push('OrderdetailPage')
-  }
-
-  gotoTestPage(){
-    this.navCtrl.push('TestpagePage')
   }
 
 }
