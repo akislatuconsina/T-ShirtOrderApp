abd9f3ec5ed9e1389f5f7ada016ff7724ea8a860
wira <swira110@gmail.com>
2017-12-19 00:23:00 +0700

    add new page (management and admin)

---
+++ src/pages/status-order/status-order.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { StatusOrderPage } from './status-order';
+
+@NgModule({
+  declarations: [
+    StatusOrderPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(StatusOrderPage),
+  ],
+})
+export class StatusOrderPageModule {}
