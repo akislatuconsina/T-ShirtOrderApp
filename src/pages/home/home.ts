15098666ea5242da82a51e674c987fc015b8175e
DESKTOP-C699GHK\HARYANTO SETYAWAN <haryantoset14@gmail.com>
2017-12-20 03:02:14 +0700

    login done

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,35 +7,29 @@ import { IonicPage, NavController, NavParams, MenuController } from 'ionic-angul
   templateUrl: 'home.html'
 })
 export class HomePage {
-   public username:any;
-  //  public activeMenu: string;
+  public username: any;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public menu: MenuController
   ) {
-    this.menu.enable (false);
-    this.menu.enable (true);
-
-    // this.activeMenu() ;
-    // this.menu.enable(activeMenu);
-    // this.menu.enable(false);
+    this.menu.enable(true);
     // this.username = this.navParams.get('name'); //fungsi ini digunakan untuk menarik data variable 
   }
   login() {
     this.navCtrl.push('LoginPage');
   }
 
-  gotoOrderPage(){
+  gotoOrderPage() {
     this.navCtrl.push('OrderPage')
-  } 
+  }
 
-  gotoDetailOrder(){
+  gotoDetailOrder() {
     this.navCtrl.push('OrderdetailPage')
   }
 
-  gotoTestPage(){
+  gotoTestPage() {
     this.navCtrl.push('TestpagePage')
   }
 
